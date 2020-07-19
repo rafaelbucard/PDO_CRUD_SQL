5a03e18dd97a577a643148103679aa5f0ecf53ef
Rafael Pereira Buçard <rafaelbucard@gmail.com>
2020-07-19 19:23:27 -0300

    Update config.php

--- config.php
+++ config.php
@@ -3,5 +3,6 @@ $db_name = 'test';
 $db_host = 'localhost';
 $db_user = 'root';
 $db_pass = '';
-$pdo  = new PDO ("mysql:dbname=".$db_name.";host=".$db_host,$db_user,$db_pass);// Informaçoes para conexão ao banco de dados 
+$pdo  = new PDO ("mysql:dbname=".$db_name.";host=".$db_host,$db_user,$db_pass);
+// Informaçoes para conexão ao banco de dados 
 
